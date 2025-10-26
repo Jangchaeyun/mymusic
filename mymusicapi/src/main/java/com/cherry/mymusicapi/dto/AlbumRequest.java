cec2ddcf1ab9011c9fc22c4376f7ae22e29ad6b3
Jangchaeyun <zzangsally@naver.com>
2025-10-26 23:06:52 +0900

    AlbumService & AlbumRepository & AlbumRequest & CloudinaryConfig

---
+++ mymusicapi/src/main/java/com/cherry/mymusicapi/dto/AlbumRequest.java
@@ -0,0 +1,20 @@
+package com.cherry.mymusicapi.dto;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AlbumRequest {
+	private String id;
+	private String name;
+	private String desc;
+	private String bgColor;
+	private MultipartFile imageFile;
+}
