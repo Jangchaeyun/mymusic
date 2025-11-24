669df303e412ddebf67199eb0cf1d512d13d4102
Jangchaeyun <zzangsally@naver.com>
2025-11-24 21:56:14 +0900

    1124 AlbumItem.jsx & Display.jsx & SongItem.jsx

--- mymusic-app/src/components/DisplayHome.jsx
+++ mymusic-app/src/components/DisplayHome.jsx
@@ -1,20 +1,42 @@
-import { useAuth } from "../context/AuthContext";
+import { useContext } from "react";
+import { PlayerContext } from "../context/PlayContext";
+import AlbumItem from "./AlbumItem";
+import SongItem from "./SongItem";
 
 const DisplayHome = () => {
+  const { songsData, albumsData } = useContext(PlayerContext);
+  console.log(albumsData);
+
   return (
     <>
       <div className="mb-4">
         <h1 className="my-5 font-bold text-2xl">추천 차트</h1>
         <div className="flex overflow-auto">
           {/* Display the albums data */}
-          앨범 표시
+          {albumsData.map((item, index) => (
+            <AlbumItem
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              id={item._id}
+              image={item.imageUrl}
+            />
+          ))}
         </div>
       </div>
       <div className="mb-4">
         <h1 className="my-5 font-bold text-2xl">오늘의 가장 큰 히트곡</h1>
         <div className="flex overflow-auto">
           {/* Display the songs data */}
-          노래 표시
+          {songsData.map((item, index) => (
+            <SongItem
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              id={item._id}
+              image={item.image}
+            />
+          ))}
         </div>
       </div>
     </>
