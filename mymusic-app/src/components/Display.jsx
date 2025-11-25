4e8aeb0d045b94b558fe82d0b09ad317771ae573
Jangchaeyun <zzangsally@naver.com>
2025-11-25 21:58:21 +0900

    1125 Display.jsx & DisplayAlbum.jsx

--- mymusic-app/src/components/Display.jsx
+++ mymusic-app/src/components/Display.jsx
@@ -1,20 +1,49 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import DisplayHome from "./DisplayHome";
 import Search from "./Search";
 import DisplayAlbum from "./DisplayAlbum";
 import Navbar from "./Navbar";
+import { useContext, useEffect, useRef } from "react";
+import { PlayerContext } from "../context/PlayContext";
 
 const Display = () => {
+  const { albumsData } = useContext(PlayerContext);
+  const displayRef = useRef();
+  const location = useLocation();
+  const isAlbum = location.pathname.includes("album");
+  const albumId = isAlbum ? location.pathname.split("/").pop() : "";
+  const bgColor = isAlbum
+    ? albumsData.find((x) => x._id == albumId).bgColour
+    : "#121212";
+
+  useEffect(() => {
+    if (isAlbum) {
+      displayRef.current.style.background = `linear-gradient(${bgColor}, #121212)`;
+    } else {
+      displayRef.current.style.background = "#121212";
+    }
+  }, [isAlbum, bgColor]);
+
   return (
     <div className="w-[100%] m-2 bg-[#121212] text-white lg:w-[75%] lg:ml-0 flex flex-col">
       {/* Sticky navbar */}
-      <div className="sticky top-0 z-10 bg-[#121212]/95 backdrop:blur-sm border-b border-gray-800/50 px-6 pt-4 pb-2">
+      <div
+        ref={displayRef}
+        className="sticky top-0 z-10 bg-[#121212]/95 backdrop:blur-sm border-b border-gray-800/50 px-6 pt-4 pb-2"
+      >
         <Navbar />
         {/* Scrollable content */}
         <div className="flex-1 px-6 pb-4 overflow-auto">
           <Routes>
             <Route path="/" element={<DisplayHome />} />
-            <Route path="/album" element={<DisplayAlbum />} />
+            <Route
+              path="/album/:id"
+              element={
+                <DisplayAlbum
+                  album={albumsData.find((x) => x._id == albumId)}
+                />
+              }
+            />
             <Route path="/search" element={<Search />} />
           </Routes>
         </div>
--- mymusic-app/src/components/DisplayAlbum.jsx
+++ mymusic-app/src/components/DisplayAlbum.jsx
@@ -1,5 +1,18 @@
-const DisplayAlbum = () => {
-  return <div>Displaying the albums</div>;
+import { useContext, useState } from "react";
+import { useParams } from "react-router-dom";
+import { PlayerContext } from "../context/PlayContext";
+
+const DisplayAlbum = ({ album }) => {
+  const { id } = useParams();
+  const { albumsData, songsData } = useContext(PlayerContext);
+
+  return albumsData ? (
+    <>
+      <div className="mt-10 flex gap-8 flex-col md:flex-row md:items-end">
+        <img src={album.imageUrl} alt="" className="w-48 rounded" />
+      </div>
+    </>
+  ) : null;
 };
 
 export default DisplayAlbum;
